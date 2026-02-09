e6e2d1af51a654fca3d2daef0b9325b8c1131ae2
User <kirilvasilev00@gmail.com>
2026-02-10 01:33:40 +0200

    Hi

--- src/model/DeliveryRoute.java
+++ src/model/DeliveryRoute.java
@@ -1,5 +1,28 @@
 package model;
 
+import exceptions.InvalidInputInfo;
+import model.enums.Location;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DeliveryRoute {
+
+    private final int id;
+    private static int counter = 1;
+    private final List<RouteStop> stops;
+    private Integer assignedTruckId;
+    final List<DeliveryPackage> packages;
+    int totalDistanceKm;
+
+    public DeliveryRoute(List<RouteStop> stops, Integer assignedTruckId, List<DeliveryPackage> packages, int totalDistanceKm){
+        this.id = counter;
+        counter++;
+        if (stops.size() < 2){throw new InvalidInputInfo("There must be at least 2 stops.");}
+        this.stops = new ArrayList<>(stops);
+        this.assignedTruckId = assignedTruckId;
+        this.packages = new ArrayList<>(packages);
+        this.totalDistanceKm = totalDistanceKm;
+    }
 
 }
--- src/model/RouteStop.java
+++ src/model/RouteStop.java
@@ -1,4 +1,25 @@
 package model;
 
+import model.enums.Location;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class RouteStop {
+    private final Location city;
+    private final LocalDateTime time;
+
+    public RouteStop(Location city, LocalDateTime time){
+        this.city=city;
+        this.time = time;
+
+    }
+
+    public Location getCity(){
+        return city;
+    }
+
+    public LocalDateTime getTime(){
+        return time;
+    }
 }
